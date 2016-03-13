0bf5481755185eb7db72fea6ba6292cc9e6423f6
hycgreen <hycgreen@gmail.com>
2016-03-13 11:52:52 +0800

    第2個紅燈
    增加情境2
    
    Scenario: 第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190
    Given 第一集買了 1 本
    And 第二集買了 1 本
    And 第三集買了 0 本
    And 第四集買了 0 本
    And 第五集買了 0 本
    When 結帳
    Then 價格應為 190 元

--- day2-homework/PotterShoppingCartTests/UnitTest1.cs
+++ day2-homework/PotterShoppingCartTests/UnitTest1.cs
@@ -26,5 +26,26 @@ namespace PotterShoppingCartTests
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_Vol1_is_1_Vol2_is_1_Vol3_is_0_Vol4_is_0_Vol5_is_0_Sum_Should_Be_190()
+        {
+            //arrange
+            var products = new List<Product>()
+                            {
+                                new Product() { Name = "HP1", Price = 100 },
+                                new Product() { Name = "HP2", Price = 100 }
+                            };
+
+            PotterShoppingCart target = new PotterShoppingCart();
+
+            //act
+            double actual = target.Sum(products);
+            double expected = 190;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
